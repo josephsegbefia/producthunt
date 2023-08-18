aec0e82753ee883f850dc32333568f245576ff2a
Joseph Segbefia <elorma.segbefia@gmail.com>
2023-08-18 12:11:31 +0200

    Initial Commit

---
+++ src/components/Product.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Product extends React.Component {
+  render() {
+    return (
+      <div className="item">
+        <div className="image">
+          <img src="../images/products/image-aqua.png" alt="aqua image" />
+        </div>
+      </div>
+    );
+  }
+}
---
+++ src/components/ProductList.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+class ProductList extends React.Component {
+  render() {
+    return (
+      <div className="ui unstackable items">
+        Hello, I will be the product list again
+      </div>
+    );
+  }
+}
+
+export default ProductList;
